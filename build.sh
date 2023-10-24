21e017f7bb63b90d822a2c630a03fb168210804e
Harsh Gupta <guptaharsh615@gmail.com>
2023-10-24 14:49:49 +0530

    sort

---
+++ build.sh
@@ -0,0 +1,8 @@
+apt update
+apt install -y docker.io
+systemctl start docker
+systemctl enable docker
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
